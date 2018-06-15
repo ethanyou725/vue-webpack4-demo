c2785c7b852f6e52874c8625fe073a7c49ef15b1
youzhigang <youzhigang@suiyi.com.cn>
2018-06-15 10:44:11 +0800

    beatutify proxy output

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -81,7 +81,7 @@ module.exports = new Promise((resolve, reject) => {
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
           messages: [`Your application is running here: http://localhost:${port}`],
-          notes: [`${JSON.stringify(config.dev.proxyTable)}`]
+          notes: [`\r\n${JSON.stringify(config.dev.proxyTable, null, 4)}`]
         },
         onErrors: config.dev.notifyOnErrors
         ? utils.createNotifierCallback()
